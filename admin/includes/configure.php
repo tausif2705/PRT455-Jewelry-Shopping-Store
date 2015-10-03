e56c2e99e6faa59613b2d01d997a719989d56b55
tausif2705 <nurunmukhi@yahoo.com>
2015-10-03 15:04:35 +0930

    modifies check out
    testing finished

---
+++ admin/includes/configure.php
@@ -0,0 +1,43 @@
+<?php
+  define('HTTP_SERVER', 'http://highflextech.com');
+  define('HTTPS_SERVER', 'http://highflextech.com');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/osc/osc2/admin');
+  define('HTTPS_COOKIE_PATH', '/osc/osc2/admin');
+  define('HTTP_CATALOG_SERVER', 'http://highflextech.com');
+  define('HTTPS_CATALOG_SERVER', 'http://highflextech.com');
+  define('ENABLE_SSL_CATALOG', 'false');
+  define('DIR_FS_DOCUMENT_ROOT', '/home/highflextech/public_html/osc/osc2/');
+  define('DIR_WS_ADMIN', '/osc/osc2/admin/');
+  define('DIR_WS_HTTPS_ADMIN', '/osc/osc2/admin/');
+  define('DIR_FS_ADMIN', '/home/highflextech/public_html/osc/osc2/admin/');
+  define('DIR_WS_CATALOG', '/osc/osc2/');
+  define('DIR_WS_HTTPS_CATALOG', '/osc/osc2/');
+  define('DIR_FS_CATALOG', '/home/highflextech/public_html/osc/osc2/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+  define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_IMAGES', DIR_FS_CATALOG . 'images/');
+  define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
+  define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'highflex_oscuser');
+  define('DB_SERVER_PASSWORD', 'highflex_oscuser');
+  define('DB_DATABASE', 'highflex_osc2');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'Australia/Darwin');
+?>
---
+++ includes/configure.php
@@ -0,0 +1,31 @@
+<?php
+  define('HTTP_SERVER', 'http://highflextech.com');
+  define('HTTPS_SERVER', 'http://highflextech.com');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/osc/osc2/');
+  define('HTTPS_COOKIE_PATH', '/osc/osc2/');
+  define('DIR_WS_HTTP_CATALOG', '/osc/osc2/');
+  define('DIR_WS_HTTPS_CATALOG', '/osc/osc2/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/home/highflextech/public_html/osc/osc2/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'highflex_oscuser');
+  define('DB_SERVER_PASSWORD', 'highflex_oscuser');
+  define('DB_DATABASE', 'highflex_osc2');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'Australia/Darwin');
+?>
